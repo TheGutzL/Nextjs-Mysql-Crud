85af0467af485cd29d51270704f5f087b506aeab
Gutz <darckProyect8@gmail.com>
2024-08-12 16:40:15 -0500

    Subir imagen de manera local

--- src/app/api/products/route.ts
+++ src/app/api/products/route.ts
@@ -1,6 +1,8 @@
 import { conn } from "@/libs/db";
 import Error from "next/error";
 import { NextResponse } from "next/server";
+import { writeFile } from 'fs/promises'
+import path from 'path'
 
 export async function GET() {
   try {
@@ -19,18 +21,34 @@ export async function GET() {
 
 export async function POST(request: Request) {
   try {
-    const { name, description, price } = await request.json();
+    const data = await request.formData();
+    const image: File | null = data.get("image") as File;
+
+    if (!image) {
+      return NextResponse.json(
+        {
+          message: "Image is required",
+        },
+        { status: 400 }
+      );
+    }
+
+    const bytes = await image.arrayBuffer()
+    const buffer = Buffer.from(bytes)
+
+    const filePath = path.join(process.cwd(), 'public', image.name)
+    await writeFile(filePath, buffer);
 
     const result: any = await conn.query("INSERT INTO product SET ?", {
-      name,
-      description,
-      price,
+      name: data.get("name"),
+      description: data.get("description"),
+      price: data.get("price"),
     });
 
     return NextResponse.json({
-      name,
-      description,
-      price,
+      name: data.get("name"),
+      description: data.get("description"),
+      price: data.get("price"),
       id: result.insertId,
     });
   } catch (error: Error | any) {
--- src/components/ProductForm/ProductForm.tsx
+++ src/components/ProductForm/ProductForm.tsx
@@ -10,6 +10,7 @@ const ProductForm = () => {
     price: 0,
     description: "",
   });
+  const [file, setFile] = useState<File | null>(null);
   const form = useRef<HTMLFormElement>(null);
   const router = useRouter();
   const params = useParams();
@@ -40,7 +41,19 @@ const ProductForm = () => {
     e.preventDefault();
 
     if (!params.id) {
-      const res = await axios.post("/api/products", product);
+      const formData = new FormData();
+      formData.append("name", product.name);
+      formData.append("price", product.price.toString());
+      formData.append("description", product.description);
+      if (file) {
+        formData.append("image", file);
+      }
+
+      const res = await axios.post("/api/products", formData, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      });
     } else {
       const res = await axios.put(`/api/products/${params.id}`, product);
     }
@@ -53,72 +66,86 @@ const ProductForm = () => {
   };
 
   return (
-    <form
-      className="bg-white shadow-md rounded-md px-8 pt-6 pb-8 mb-4"
-      onSubmit={handleSubmit}
-      ref={form}
-    >
-      <label
-        className="block text-gray-700 text-sm font-bold mb-2"
-        htmlFor="name"
+    <div className="flex ">
+      <form
+        className="bg-white shadow-md rounded-md px-8 pt-6 pb-8 mb-4"
+        onSubmit={handleSubmit}
+        ref={form}
       >
-        Product Name:
-      </label>
-      <input
-        name="name"
-        type="text"
-        placeholder="Name"
-        onChange={handleChange}
-        value={product.name}
-        className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700"
-        autoFocus
-      />
+        <label
+          className="block text-gray-700 text-sm font-bold mb-2"
+          htmlFor="name"
+        >
+          Product Name:
+        </label>
+        <input
+          name="name"
+          type="text"
+          placeholder="Name"
+          onChange={handleChange}
+          value={product.name}
+          className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700"
+          autoFocus
+        />
 
-      <label
-        className="block text-gray-700 text-sm font-bold mb-2"
-        htmlFor="price"
-      >
-        Product Price:
-      </label>
-      <input
-        name="price"
-        type="number"
-        placeholder="Price"
-        onChange={handleChange}
-        value={product.price}
-        className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700"
-      />
+        <label
+          className="block text-gray-700 text-sm font-bold mb-2"
+          htmlFor="price"
+        >
+          Product Price:
+        </label>
+        <input
+          name="price"
+          type="number"
+          placeholder="Price"
+          onChange={handleChange}
+          value={product.price}
+          className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700"
+        />
 
-      <label
-        className="block text-gray-700 text-sm font-bold mb-2"
-        htmlFor="description"
-      >
-        Product Descripcion:
-      </label>
-      <textarea
-        name="description"
-        rows={3}
-        placeholder="Description"
-        onChange={handleChange}
-        value={product.description}
-        className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700"
-      />
+        <label
+          className="block text-gray-700 text-sm font-bold mb-2"
+          htmlFor="description"
+        >
+          Product Descripcion:
+        </label>
+        <textarea
+          name="description"
+          rows={3}
+          placeholder="Description"
+          onChange={handleChange}
+          value={product.description}
+          className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700"
+        />
 
-      {/* <label
-        className="block text-gray-700 text-sm font-bold mb-2"
-        htmlFor="image"
-      >
-        Product Image:
-      </label>
-      <input
-        type="file"
-        className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-2"
-      /> */}
+        <label
+          className="block text-gray-700 text-sm font-bold mb-2"
+          htmlFor="image"
+        >
+          Product Image:
+        </label>
+        <input
+          type="file"
+          className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-2"
+          onChange={(e) => {
+            if (e.target.files) {
+              setFile(e.target.files[0]);
+            }
+          }}
+        />
+        {file && (
+          <img
+            src={URL.createObjectURL(file)}
+            alt="file"
+            className="w-96 object-contain mx-auto my-4"
+          />
+        )}
 
-      <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold px-4 py-2 rounded">
-        {params.id ? "Update Product" : "Create Product"}
-      </button>
-    </form>
+        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold px-4 py-2 rounded">
+          {params.id ? "Update Product" : "Create Product"}
+        </button>
+      </form>
+    </div>
   );
 };
 
